01d900d5d6a7240db43bdca540a54cce6bbad5b8
theeValeyard <ttamanna1@northislandcollege.ca>
2023-10-18 02:25:12 -0700

    Add html code to arrays.php

---
+++ websites/demo/arrays.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+
+<body>
+    <h1> Recommended Books</h1>
+
+    <ul>
+        <li>Do Androids Dream of Electric Sheep</li>
+        <li>The Langoliers</li>
+        <li>Hail Mary</li>
+    </ul>
+</body>
+
+</html>

291dff70f169b3fe5d654d59bb5ed260d4024ed9
theeValeyard <ttamanna1@northislandcollege.ca>
2023-10-18 02:33:10 -0700

    Show array items as a list

--- websites/demo/arrays.php
+++ websites/demo/arrays.php
@@ -11,14 +11,16 @@
 
     <?php 
     $books = [
-        "Do Androids Dream of Electric Sheep"
-        "The Langoliers"
+        "Do Androids Dream of Electric Sheep",
+        "The Langoliers",
         "Hail Mary"
     ];
     ?>
 
     <ul>
-        <?php 
+        <?php foreach ($books as $book) {
+            echo "<li>" . $book . "</li>";
+        }
         ?>
     </ul>
 </body>

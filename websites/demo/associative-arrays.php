028bc21ef1c7fda815b03df1827c0de96344e148
theeValeyard <ttamanna1@northislandcollege.ca>
2023-10-18 03:10:13 -0700

    07 (Associative arrays) complete

--- websites/demo/associative-arrays.php
+++ websites/demo/associative-arrays.php
@@ -8,30 +8,36 @@
 
 <body>
 
-<?php 
-$books = [
-   ['name' => 'Do Androids Dream of Electric Sheep',
-   'author' => 'Philip K. Dick',
-   'purchaseUrl' => 'http://example.com'],
+    <?php
+    $books = [
+        [
+            'name' => 'Do Androids Dream of Electric Sheep',
+            'author' => 'Philip K. Dick',
+            'purchaseUrl' => 'http://example.com'
+        ],
 
-    ['name' => 'The Langoliers',
-   'author' => 'Andy Weir',
-   'purchaseUrl' => 'http://example.com',],
+        [
+            'name' => 'The Langoliers',
+            'author' => 'Andy Weir',
+            'purchaseUrl' => 'http://example.com',
+        ],
 
-    ['name' => 'Hail Mary',
-    'author' => 'Philip K. Dick',
-    'purchaseUrl' => 'http://example.com']
-];
-?>
- <ul>
-        <?php foreach ($books as $book): ?> 
+        [
+            'name' => 'Hail Mary',
+            'author' => 'Philip K. Dick',
+            'purchaseUrl' => 'http://example.com'
+        ]
+    ];
+    ?>
+    <ul>
+        <?php foreach ($books as $book) : ?>
             <li>
-                <a href="<?= $book['purchaseUrl'] ?>">
-                <?= $book ['name'] ?>
+                <a href="<?= $book['purchaseUrl']; ?>">
+                    <?= $book['name']; ?>
                 </a>
             </li>
         <?php endforeach; ?>
- </ul>
+    </ul>
 </body>
 
 </html>

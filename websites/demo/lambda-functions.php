dafc958dd4e823cf559f5e7bd3f03db3bb1e141f
theeValeyard <ttamanna1@northislandcollege.ca>
2023-10-18 05:23:45 -0700

    create a lambda function

---
+++ websites/demo/lambda-functions.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+
+<body>
+
+    <?php
+    $books = [
+        [
+            'name' => 'Do Androids Dream of Electric Sheep',
+            'author' => 'Philip K. Dick',
+            'releaseYear' => 1968,
+            'purchaseUrl' => 'http://example.com'
+        ],
+
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2021,
+            'purchaseUrl' => 'http://example.com',
+        ],
+
+        [
+            'name' => 'The Martian',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2011,
+            'purchaseUrl' => 'http://example.com'
+        ]
+    ];
+
+    $filterByAuthor = function ($books, $author) {
+        $filteredBooks = [];
+
+        foreach ($books as $book) {
+            if ($book['author'] === $author) {
+                $filteredBooks[] = $book;
+            }
+        }
+
+        return $filteredBooks;
+    };
+    $filteredBooks = $filterByAuthor($books, 'Philip K. Dick');
+    ?>
+
+    <ul>
+        <?php foreach ($filteredBooks as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl']; ?>">
+                    <?= $book['name']; ?> ( <?= $book['releaseYear']; ?>) - By <?= $book['author']; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+</body>
+
+</html>

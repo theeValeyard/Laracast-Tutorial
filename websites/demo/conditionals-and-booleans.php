fb584595b676b46c6a6528e3b968ca2f6a56b891
theeValeyard <ttamanna1@northislandcollege.ca>
2023-10-18 02:04:03 -0700

    Add Css to page

---
+++ websites/demo/conditionals-and-booleans.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>
+   You have read "Dark Matter."
+    </h1>
+</body>
+
+</html>
